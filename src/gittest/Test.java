1b562c7d537b3110089f277a298c28d04607a102
renchunbing <renchunbing@duia.com>
2015-04-17 16:23:14 +0800

    remove out

--- src/gittest/Test.java
+++ src/gittest/Test.java
@@ -5,6 +5,5 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hellow Git");
 		System.out.println("GitHub change");
-		System.out.println("Git Remote push");
 	}
 }
