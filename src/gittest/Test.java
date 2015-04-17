b8633d9db3f3b35d5fbbc7a942bc1067c205926b
renchunbing <renchunbing@duia.com>
2015-04-17 15:32:13 +0800

    Version4.0

--- src/gittest/Test.java
+++ src/gittest/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hellow Git");
 		System.out.println("GitHub change");
+		System.out.println("Git Remote push");
 	}
 }
